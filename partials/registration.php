db4c97558dbcef1fff27fb70350e3d00b1994241
Puskar <puskarbha@gmail.com>
2022-05-30 22:26:38 +0545

    added registration page

--- partials/registration.php
+++ partials/registration.php
@@ -7,7 +7,38 @@
     <title>Voting System-Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark" >
+    <h1 class="text-center text-info p-3">Voting System</h1>
+    <div class="bg-info py-4">
+        <h2 class="text-center">Register Account</h2>
+        <div class="container text-center" >
+            <form action="">
+                <div class="mb-3">
+                    <input type="text" class="form-control w-50 m-auto" placeholder="Enter your username." required="required" name="username" >
+                </div>
+                <div class="mb-3">
+                    <input type="text" class="form-control w-50 m-auto" placeholder="Enter your mobile number." required="required" name="mobile" >
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control w-50 m-auto" placeholder="Enter your password" required="required" name="password" >
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control w-50 m-auto" placeholder="Confirm your password" required="required" name="cpassword" >
+                </div>
+                <div class="mb-3">
+                    <input type="file" class="form-control w-50 m-auto"  name="photo" >
+                </div>
+                <div class="mb-3">
+                    <select name="std" class="form-select w-50 m-auto">
+                        <option value="group">Group</option>]
+                        <option value="voter">Voter</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-dark my-4">Register</button>
+                <p>Already have a account?<a href="../" class="text-white">Login here</a></p>
+            </form>
+        </div>
+    </div>
    
 </body>
 </html>
