32eb1875b92ae673959f44ce1544b1473520e387
Puskar <puskarbha@gmail.com>
2022-05-31 11:01:50 +0545

    added dashboard layout

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -12,6 +12,32 @@
     <div class="container my-5">
         <a href="../"><button class="btn btn-dark text-light px-3">Back</button></a>
         <a href="logout.php"><button class="btn btn-dark text-light px-3">Logout</button></a>
+        <h1 class="my-3">Voting System</h1>
+        <div class="row my-5">
+            <!--Groups-->
+            <div class="col-md-7">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="" alt="Group image">
+                    </div>
+                    <div class="col-md-8">
+                        <strong class="text-dark h5"> Group Name:</strong><br>
+                        <strong class="text-dark h5"> Votes:</strong><br>
+                     
+                    </div>
+                </div>
+                <hr>
+            </div>
+
+            <!-- user profile -->
+            <div class="col-md-5">
+                <img src="" alt="user iamge" >
+                <br> <br>
+                <strong class="text-dark h5"> Name:</strong><br><br>
+                <strong class="text-dark h5"> Mobile:</strong><br><br>
+                <strong class="text-dark h5"> Status:</strong><br><br>
+            </div>
+        </div>
     </div>
 </body>
 </html>
